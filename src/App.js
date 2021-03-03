4fb71ce549fbcf311a90629a165dea16ad06da8e
Alice <alicewrites@hotmail.com>
2021-03-03 23:49:57 +0200

    Added GitHub repository URL

--- src/App.js
+++ src/App.js
@@ -4,5 +4,13 @@ import './App.css';
 
 
 export default function App() {
-  return <div className="App"> hi </div>;
+  return <div className="App">
+     <h1> Weather App Alice 2021 </h1>
+     <footer>
+       Coded by Alice Gitman{" "}
+     <a href="https://github.com/AliceCod3s/alice-project-react" target="_blank">
+open-sourced in GitHub
+     </a>
+     </footer>
+      </div>;
   }
